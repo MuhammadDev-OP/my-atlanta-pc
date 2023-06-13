834ab09aab8321c26dc37654b6a7136411dabe69
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-06-13 21:01:04 +0500

    ftcggf

---
+++ src/app/components/modals/Modal.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void
+
+}
+
+
+export const Modal= = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Modal;
