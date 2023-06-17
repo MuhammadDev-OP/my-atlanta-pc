cf061a75b6c8c0a15a566e2d0713600bace45a1f
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-06-17 09:33:28 +0500

    Commitedd

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,11 +1,18 @@
 'use client'
-
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import { UserMenu } from "./UserMenu";
+import { SafeUser } from "@/app/types";
 
-export default function Navbar() {
+interface NavbarProps {
+  currentUser?: SafeUser | null
+}
+
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser
+
+}) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
@@ -13,10 +20,12 @@ export default function Navbar() {
           <div className="flex flex-row items-center justify-between gap-3 md-gap-0">
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
     </div>
   );
 }
+
+export default Navbar;
